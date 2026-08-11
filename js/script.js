db6030382b6b7885cedb7e2cce7e12f98038defc
msx993 <mivanovych2012@gmail.com>
2026-08-11 11:18:51 +0300

    я

--- js/script.js
+++ js/script.js
@@ -1,26 +1,3 @@
-const colorPalette = document.querySelector(".color-palette");
-const output = document.querySelector(".output");
-
-const createPaletteItems = () => {
-    const buttons = []
-for (let i = 0; i < 36; i+=1) {
-    const button = document.createElement("button")
-    button.classList.add("item")
-    button.type = "button"
-    const color = gatRandomHexColor()
-    button.style.backgroundColor = color
-    button.push(button)
-}
-}
-function getRandomHexColor() {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-  
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
 
 
 const colorPalette = document.querySelector(".color-palette");
@@ -65,3 +42,31 @@ function getRandomHexColor() {
     }
     return color;
 }
+
+
+const userList = document.querySelector("#userList");
+const onDeleteButtonClick = (event) => {
+    if (event.target.nodeName !== "BUTTON") {
+        return
+    }
+const button = event.target
+const li = button.parentNode
+const id = li.dataset.id
+const itemToDelete = document.querySelector(`li[data-id="${id}"]`)
+itemToDelete.remove()
+}
+userList.addEventListener("click", onDeleteButtonClick)
+
+
+
+const navList = document.querySelector("js-nav")
+const onNavClick = (event) => {
+
+}
+const activateButton = event.target
+activateButton.classList.add('active')
+const currentActiveButton = navList .querySelector(".btn .activate")
+
+
+
+navList.addEventListener("click", onNavClick)
